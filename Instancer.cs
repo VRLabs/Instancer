d2491cbfd20e11901f0e3fb5edf10ffa3924786d
jellejurre <jelle@jilles.com>
2024-02-25 22:32:54 +0100

    Remove unneeded import

--- Instancer.cs
+++ Instancer.cs
@@ -1,10 +1,9 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
-using JetBrains.Annotations;
 using UnityEditor;
 using UnityEngine;
 using Object = UnityEngine.Object;
